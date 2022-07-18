d46bba1ac82ea61a8a8d1045d16a11da0673d74c
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-07-18 11:42:11 +0300

    customer interface & customers-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './root/app.component';
+import { CustomersPageComponent } from './pages/Customers/customers-page/customers-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CustomersPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
